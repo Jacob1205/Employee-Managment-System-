d90757ff6dd1736f4e1dbfaa63860416aa5d0a36
Jacob Torres <simplyjacob00@gmail.com>
2020-10-20 17:26:35 -0400

    database

---
+++ app.js
@@ -0,0 +1,20 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require("console.table");
+const pw = require("./pw");
+const chalk = require("chalk");
+// Create connection to mysql db
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: pw,
+  database: "employee_manager",
+});
+// Connecting to the db
+connection.connect((err) => {
+  if (err) throw err;
+  // Logs CPU thread to console
+  console.log(`\n \n Connected on thread: ${connection.threadId}`);
+  initPrompts();
+});
